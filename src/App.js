26214c70210cd34e84aec509c13c522904db1a5c
Michael Trew <mtrew2015@gmail.com>
2022-03-10 16:04:30 -0500

    add landing page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import { Container } from './App.styles';
-import NavBar from './organisms/navbar/NavBar';
+import {NavBar} from './organisms/NavBar/NavBar';
+import { Routes, Route } from 'react-router-dom/';
+import {LandingPage} from './pages/LandingPage/LandingPage'
 
 function App() {
   return (
     <Container>
-      <NavBar/>
+      <NavBar />
+      <Routes>
+        <Route exact path="/" element={<LandingPage/>}/>
+      </Routes>
+
+
     </Container>
   );
 }
