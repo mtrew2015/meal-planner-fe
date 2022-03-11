0bf68337b9a2041d6744f5f8e758023db80c665a
Michael Trew <mtrew2015@gmail.com>
2022-03-11 15:45:30 -0500

    Adding Sidebar

--- src/organisms/NavBar/NavBar.js
+++ src/organisms/NavBar/NavBar.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavContainer } from './NavBar.styles';
+import { Drawer } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
+import SideBar from '../SideBar/SideBar';
 
 export const NavBar = () => {
+  const [open, setOpen] = useState(false);
   return (
     <NavContainer>
-      <MenuIcon fontSize='large' />
+      <MenuIcon onClick={() => setOpen(true)} fontSize='large' />
       <div>
         <h1>Meal Planner</h1>
       </div>
-      <div></div>
+      <div>
+        <Drawer anchor='left' open={open} onClose={() => setOpen(false)}>
+          <SideBar />
+        </Drawer>
+      </div>
     </NavContainer>
   );
 };
---
+++ src/organisms/SideBar/SideBar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Container } from './SideBar.styles';
+
+export default function SideBar() {
+  return (
+    <Container>
+      <h1>Sidebar</h1>
+    </Container>
+  );
+}
