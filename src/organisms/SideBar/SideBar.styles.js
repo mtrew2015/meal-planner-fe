5ef918ca2dd8ff0a26bf62b38b7c4ceea8f8492c
Michael Trew <mtrew2015@gmail.com>
2022-03-12 11:54:16 -0500

    adding styles

--- src/organisms/SideBar/SideBar.styles.js
+++ src/organisms/SideBar/SideBar.styles.js
@@ -3,4 +3,10 @@ export const Container = styled.div`
   width: 200px;
   background-color: black;
   height: 100%;
+  color: white;
+  padding-right: 10px;
+  padding-left: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
