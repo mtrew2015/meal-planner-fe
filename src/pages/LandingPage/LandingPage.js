314c76206a0c35c29b62ef93a066316d7fdee954
Michael Trew <mtrew2015@gmail.com>
2022-03-14 11:04:09 -0400

    query working but commenting out for now

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import {getWeekOfYear} from '../../util/dateHelpers'
+import {useLandingPage} from '../LandingPage/useLandingPage'
 
 export const LandingPage = () => {
+  // const {data} = useLandingPage()
   return (
     <div>LandingPage</div>
   )
--- src/pages/LandingPage/useLandingPage.js
+++ src/pages/LandingPage/useLandingPage.js
@@ -1,5 +1,20 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
+import { useMealPlansQuery } from '../../generated/graphql.tsx';
+import { getWeekOfYear } from '../../util/dateHelpers';
 
 export const useLandingPage = () => {
+  const weekOfYear = getWeekOfYear();
+  const { data, loading, error } = useMealPlansQuery({
+    variables: {
+      filters: {
+        weekNumber: weekOfYear,
+      },
+    },
+  });
 
-}
+  console.log(data)
+
+  return {
+    data
+  }
+};
