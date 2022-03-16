6903113be20fbbdcc5938030d51eabc8540dad5e
Michael Trew <mtrew2015@gmail.com>
2022-03-16 16:00:35 -0400

    add date picker to other component

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -4,31 +4,42 @@ import { useLandingPage } from '../LandingPage/useLandingPage';
 import { MealPlan } from '../../organisms/MealPlan/MealPlan';
 import DesktopDatePicker from '@mui/lab/DesktopDatePicker';
 import { MealPlanForm } from '../../organisms/MealPlanForm/MealPlanForm';
-import {TextField} from '@mui/material'
+import { TextField } from '@mui/material';
 import AdapterDateFns from '@mui/lab/AdapterDateFns';
 import LocalizationProvider from '@mui/lab/LocalizationProvider';
 
-
 export const LandingPage = () => {
   const { mealPlanData, value, handleChange, week } = useLandingPage();
-  
+
   if (mealPlanData?.mealPlans?.length) {
     return (
       <div>
         <LocalizationProvider dateAdapter={AdapterDateFns}>
-        <DesktopDatePicker
-          label="Select A Week"
-          inputFormat="MM/dd/yyyy"
-          value={value}
-          onChange={handleChange}
-          renderInput={(params) => <TextField {...params} />}
-        />
+          <DesktopDatePicker
+            label='Select A Week'
+            inputFormat='MM/dd/yyyy'
+            value={value}
+            onChange={handleChange}
+            renderInput={(params) => <TextField {...params} />}
+          />
         </LocalizationProvider>
-        LandingPage
         {mealPlanData?.mealPlans?.length && <MealPlan data={mealPlanData} />}
       </div>
     );
   } else {
-    return <MealPlanForm week={week} />;
+    return (
+      <>
+        <LocalizationProvider dateAdapter={AdapterDateFns}>
+          <DesktopDatePicker
+            label='Select A Week'
+            inputFormat='MM/dd/yyyy'
+            value={value}
+            onChange={handleChange}
+            renderInput={(params) => <TextField {...params} />}
+          />
+        </LocalizationProvider>
+        <MealPlanForm week={week} />
+      </>
+    );
   }
 };
