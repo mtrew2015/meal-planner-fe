0e6f7a3e22aef5fe4f66a435200e3c917f4828e9
Michael Trew <mtrew2015@gmail.com>
2022-03-15 20:46:49 -0400

    changing navbar styles

--- src/organisms/NavBar/NavBar.js
+++ src/organisms/NavBar/NavBar.js
@@ -2,15 +2,17 @@ import React, { useState } from 'react';
 import { NavContainer } from './NavBar.styles';
 import { Drawer } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
+import RestaurantIcon from '@mui/icons-material/Restaurant';
 import SideBar from '../SideBar/SideBar';
+import './NavBar.scss';
 
 export const NavBar = () => {
   const [open, setOpen] = useState(false);
   return (
     <NavContainer>
       <MenuIcon onClick={() => setOpen(true)} fontSize='large' />
-      <div>
-        <h1>Meal Planner</h1>
+      <div className='justifyCenter'>
+        <RestaurantIcon fontSize='large' />
       </div>
       <div>
         <Drawer anchor='left' open={open} onClose={() => setOpen(false)}>
